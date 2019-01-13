487192ab83639130738e622b6ca9bee9c4ae6a77
kean <chankeanyeow@gmail.com>
2019-01-13 12:02:40 +0800

    1.0

---
+++ car_insurance_cal/src/car_insurance_cal/car.java
@@ -0,0 +1,141 @@
+package car_insurance_cal;
+
+public class car {
+	
+	public int cc;
+	public int market_value;
+	public int ncd;
+	public int windscreen_cover;
+	public int stamp=10;
+	
+	public car(int cc,int market_value,int ncd,int windscreen_cover) {
+		this.cc=cc;
+		this.market_value=market_value;
+		this.ncd=ncd;
+		this.windscreen_cover=windscreen_cover;
+		
+	}
+	
+	public int getCC() {
+		return cc;
+	}
+	
+	public int getMarketValue() {
+		return market_value;
+	}
+	
+	public int getNCD() {
+		return ncd;
+	}
+	
+	public int getStamp() {
+		return stamp;
+	}
+	
+	public int getWindscreen_cover() {
+		return windscreen_cover;
+	}
+	
+	public double getRate() {
+		cc=getCC();
+		double rate = 0;
+		if (cc<=1400) {
+			rate=273.8;
+		} else if (cc>1400 && cc<=1650) {
+			rate=305.5;
+		} else if (cc>1650 && cc<=2200) {
+			rate=339.1;
+		} else if (cc>2200 && cc<=3050) {
+			rate=372.6;
+		} else if (cc>3050 && cc<=4100) {
+			rate=404.3;
+		} else if (cc>4100 && cc<=4250) {
+			rate=436;
+		} else if (cc>4250 && cc<=4400) {
+			rate=469.6;
+		} else if (cc>4400) {
+			rate=501.3;
+		}
+		return rate;
+	}
+	
+	public double getRate2() {
+		 market_value=getMarketValue();
+		 int trate2=0;
+		 trate2=(market_value-1000)/1000;
+		 int rate2=trate2*26;
+		 return rate2;
+	}
+	
+	public double getBasicPrm() {
+		double basicPrm=getRate()+getRate2();
+		return basicPrm;
+	}
+	
+	public double getNCDAMT() {
+		int ncd=getNCD();
+		double basicPrm = getBasicPrm();
+		double ncd_amt=0;
+		if (ncd==1) {
+			ncd_amt=basicPrm*0.25;
+		} else if (ncd==2) {
+			ncd_amt=basicPrm*0.3;
+		} else if (ncd==3) {
+			ncd_amt=basicPrm*0.3833;
+		} else if (ncd==4) {
+			ncd_amt=basicPrm*0.45;
+		} else if (ncd==5) {
+			ncd_amt=basicPrm*0.55;
+		} 
+		return ncd_amt;
+	}
+	
+	
+	public double getWindscreen() {
+		double wsamt=0;
+		market_value=getMarketValue();
+		windscreen_cover=getWindscreen_cover();
+		
+		if (windscreen_cover==1) {
+		wsamt=market_value*0.0015;
+		} else {
+			wsamt=0;
+		}
+		return wsamt;
+	}
+	
+	
+	public double calGrsPrm() {
+		double grs_prm;
+		grs_prm = (getRate()+getRate2()-getNCDAMT()+getWindscreen());
+		return grs_prm;
+	}
+	
+	
+	public double cal_pay_prm() {
+		double pay_prm=0;
+		pay_prm=calGrsPrm()+(calGrsPrm()*0.06)+getStamp();
+		return pay_prm;
+	}
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
---
+++ car_insurance_cal/src/car_insurance_cal/initial.java
@@ -0,0 +1,166 @@
+package car_insurance_cal;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+public class initial extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("car insurance premium calculator");
+
+        // Create the registration form grid pane
+        GridPane gridPane = createRegistrationFormPane();
+        // Add UI controls to the registration form grid pane
+        addUIControls(gridPane);
+        // Create a scene with registration form grid pane as the root node
+        Scene scene = new Scene(gridPane, 800, 500);
+        // Set the scene in primary stage	
+        primaryStage.setScene(scene);
+        
+        primaryStage.show();
+    }
+
+
+    private GridPane createRegistrationFormPane() {
+        // Instantiate a new Grid Pane
+        GridPane gridPane = new GridPane();
+
+        // Position the pane at the center of the screen, both vertically and horizontally
+        gridPane.setAlignment(Pos.CENTER);
+
+        // Set a padding of 20px on each side
+        gridPane.setPadding(new Insets(40, 40, 40, 40));
+
+        // Set the horizontal gap between columns
+        gridPane.setHgap(10);
+
+        // Set the vertical gap between rows
+        gridPane.setVgap(10);
+
+        // Add Column Constraints
+
+        // columnOneConstraints will be applied to all the nodes placed in column one.
+        ColumnConstraints columnOneConstraints = new ColumnConstraints(100, 100, Double.MAX_VALUE);
+        columnOneConstraints.setHalignment(HPos.RIGHT);
+
+        // columnTwoConstraints will be applied to all the nodes placed in column two.
+        ColumnConstraints columnTwoConstrains = new ColumnConstraints(200,200, Double.MAX_VALUE);
+        columnTwoConstrains.setHgrow(Priority.ALWAYS);
+
+        gridPane.getColumnConstraints().addAll(columnOneConstraints, columnTwoConstrains);
+
+        return gridPane;
+    }
+
+    private void addUIControls(GridPane gridPane) {
+        // Add Header
+        Label headerLabel = new Label("car insurance premium calculator");
+        headerLabel.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+        gridPane.add(headerLabel, 0,0,2,1);
+        GridPane.setHalignment(headerLabel, HPos.CENTER);
+        GridPane.setMargin(headerLabel, new Insets(20, 0,20,0));
+
+        // Add CC Label
+        Label nameLabel = new Label("car CC : ");
+        gridPane.add(nameLabel, 0,1);
+
+        
+        // Add CC Text Field
+        TextField ccField = new TextField();
+        ccField.setPrefHeight(40);
+        gridPane.add(ccField, 1,1);
+
+
+        // Add MV Label
+        Label mvLabel = new Label("market value : ");
+        gridPane.add(mvLabel, 0, 2);
+
+        // Add MV Text Field
+        TextField mvField = new TextField();
+        mvField.setPrefHeight(40);
+        gridPane.add(mvField, 1, 2);
+
+        // Add NCD Label
+        Label ncdLabel = new Label("NCD year(1~5) : ");
+        gridPane.add(ncdLabel, 0, 3);
+
+        // Add NCD Field
+        TextField ncdField = new TextField();
+        ncdField.setPrefHeight(40);
+        gridPane.add(ncdField, 1, 3);
+        
+        // Add windscreen_cover Label
+        Label wcLabel = new Label("windscreen(1/0) opt : ");
+        gridPane.add(wcLabel, 0, 4);
+        
+        // Add windscreen_cover Text Field
+        TextField wcField = new TextField();
+        wcField.setPrefHeight(40);
+        gridPane.add(wcField, 1, 4);
+        
+        // Add Submit Button
+        Button submitButton = new Button("Submit");
+        submitButton.setPrefHeight(40);
+        submitButton.setDefaultButton(true);
+        submitButton.setPrefWidth(100);
+        gridPane.add(submitButton, 0, 5, 2, 1);
+        GridPane.setHalignment(submitButton, HPos.CENTER);
+        GridPane.setMargin(submitButton, new Insets(20, 0,20,0));
+
+        submitButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if(ccField.getText().isEmpty()) {
+                    showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), "Form Error!", "Please enter your CC");
+                    return;
+                }
+                if(mvField.getText().isEmpty()) {
+                    showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), "Form Error!", "Please enter your market value");
+                    return;
+                }
+                if(ncdField.getText().isEmpty()) {
+                    showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), "Form Error!", "Please enter a NCD year");
+                    return;
+                }
+                if(wcField.getText().isEmpty()) {
+                    showAlert(Alert.AlertType.ERROR, gridPane.getScene().getWindow(), "Form Error!", "Please enter a Windscreen option");
+                    return;
+                }
+                
+                car carObj = new car(
+                		Integer.parseInt(ccField.getText()),
+                		Integer.parseInt(mvField.getText()),
+                		Integer.parseInt(ncdField.getText()),
+                		Integer.parseInt(wcField.getText()));
+                
+                showAlert(Alert.AlertType.CONFIRMATION, gridPane.getScene().getWindow(), "compute successful!","Premium :"+Double.toString(carObj.cal_pay_prm()));
+                
+            }
+        });
+    }
+
+    private void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(owner);
+        alert.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
